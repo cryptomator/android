d3bb9d30aa177efe495250c9ecf8e3c081e956e0
Manuel Jenny <git@manueljenny.ch>
2021-03-23 12:43:08 +0100

    fix: remove cache for OkHttpClient

--- data/src/main/java/org/cryptomator/data/cloud/pcloud/PCloudClientFactory.java
+++ data/src/main/java/org/cryptomator/data/cloud/pcloud/PCloudClientFactory.java
@@ -7,11 +7,8 @@ import com.pcloud.sdk.Authenticators;
 import com.pcloud.sdk.PCloudSdk;
 
 import org.cryptomator.data.cloud.okhttplogging.HttpLoggingInterceptor;
-import org.cryptomator.util.SharedPreferencesHandler;
 import org.cryptomator.util.crypto.CredentialCryptor;
-import org.cryptomator.util.file.LruFileCacheUtil;
 
-import okhttp3.Cache;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import timber.log.Timber;
@@ -30,23 +27,18 @@ class PCloudClientFactory {
 
 	public ApiClient getClient(String accessToken, String url, Context context) {
 		if (apiClient == null) {
-			final SharedPreferencesHandler sharedPreferencesHandler = new SharedPreferencesHandler(context);
-			apiClient = createApiClient(accessToken, url, context, sharedPreferencesHandler.useLruCache(), sharedPreferencesHandler.lruCacheSize());
+			apiClient = createApiClient(accessToken, url, context);
 		}
 		return apiClient;
 	}
 
-	private ApiClient createApiClient(String accessToken, String url, Context context, boolean useLruCache, int lruCacheSize) {
+	private ApiClient createApiClient(String accessToken, String url, Context context) {
 		OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient() //
 				.newBuilder() //
 				.connectTimeout(CONNECTION.getTimeout(), CONNECTION.getUnit()) //
 				.readTimeout(READ.getTimeout(), READ.getUnit()) //
 				.writeTimeout(WRITE.getTimeout(), WRITE.getUnit()) //
 				.addInterceptor(httpLoggingInterceptor(context)); //;
-
-		if (useLruCache) {
-			okHttpClientBuilder.cache(new Cache(new LruFileCacheUtil(context).resolve(LruFileCacheUtil.Cache.PCLOUD), lruCacheSize));
-		}
 
 		OkHttpClient okHttpClient = okHttpClientBuilder.build();
 
