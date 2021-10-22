7add3e5bed596f7d4a4a73709f5fe2f7f519aebf
Julian Raufelder <Julian@Raufelder.com>
2021-10-22 17:40:08 +0200

    Delete local storage cloud and set its vault connection to null (#251)

---
+++ data/src/main/java/org/cryptomator/data/db/Upgrade9To10.kt
@@ -0,0 +1,30 @@
+package org.cryptomator.data.db
+
+import org.cryptomator.domain.CloudType
+import org.greenrobot.greendao.database.Database
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+internal class Upgrade9To10 @Inject constructor() : DatabaseUpgrade(9, 10) {
+
+	override fun internalApplyTo(db: Database, origin: Int) {
+		db.beginTransaction()
+
+		try {
+			Sql.update("VAULT_ENTITY")
+				.set("FOLDER_CLOUD_ID", Sql.toString(null))
+				.where("FOLDER_CLOUD_ID", Sql.eq(4))
+				.executeOn(db)
+
+			Sql.deleteFrom("CLOUD_ENTITY")
+				.where("FOLDER_CLOUD_ID", Sql.eq(4))
+				.where("TYPE", Sql.eq(CloudType.LOCAL.name))
+				.executeOn(db)
+
+			db.setTransactionSuccessful()
+		} finally {
+			db.endTransaction()
+		}
+	}
+}
