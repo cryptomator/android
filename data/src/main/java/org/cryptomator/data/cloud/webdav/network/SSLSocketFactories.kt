ad8a2ae747a4ceb480661a06e8a8820986ee224f
Julian Raufelder <Julian@Raufelder.com>
2021-11-04 14:56:04 +0100

    Set TLS1.3 as enabled protocol in WebDAV-client on Android >= v10
    Fixes #374

--- data/src/main/java/org/cryptomator/data/cloud/webdav/network/SSLSocketFactories.kt
+++ data/src/main/java/org/cryptomator/data/cloud/webdav/network/SSLSocketFactories.kt
@@ -11,7 +11,12 @@ internal object SSLSocketFactories {
 
 	fun from(trustManager: X509TrustManager): SSLSocketFactory {
 		return try {
-			val sslContext = SSLContext.getInstance("TLSv1.2")
+			val tlsVersion = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+				"TLSv1.3"
+			} else {
+				"TLSv1.2"
+			}
+			val sslContext = SSLContext.getInstance(tlsVersion)
 			sslContext.init(null, arrayOf<TrustManager>(trustManager), null)
 			sslContext.socketFactory
 		} catch (e: GeneralSecurityException) {
