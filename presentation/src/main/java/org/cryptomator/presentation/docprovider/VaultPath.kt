e131a8afec051ca8db763948aa2caf9abd86a6e2
JaniruTEC <52893617+JaniruTEC@users.noreply.github.com>
2023-08-28 22:05:36 +0200

    Added `#equals` and `#hashCode` to VaultPath

--- presentation/src/main/java/org/cryptomator/presentation/docprovider/VaultPath.kt
+++ presentation/src/main/java/org/cryptomator/presentation/docprovider/VaultPath.kt
@@ -20,6 +20,15 @@ class VaultPath(val vault: Vault, path: String?) {
 	val parent: VaultPath?
 		get() = if (isRoot) null else VaultPath(vault, path.substringBeforeLast('/', ""))
 
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (javaClass != other?.javaClass) return false
+		return documentId == (other as VaultPath).documentId
+	}
+
+	override fun hashCode(): Int {
+		return documentId.hashCode()
+	}
 }
 
 fun normalizePath(path: String?): String {
