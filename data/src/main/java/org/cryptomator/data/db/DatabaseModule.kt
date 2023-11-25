f40a147b9fa81ff54d0ca25d89102903bc45ff87
JaniruTEC <52893617+JaniruTEC@users.noreply.github.com>
2023-11-25 22:56:15 +0100

    Replaced asset-based template with runtime-generated file

--- data/src/main/java/org/cryptomator/data/db/DatabaseModule.kt
+++ data/src/main/java/org/cryptomator/data/db/DatabaseModule.kt
@@ -1,10 +1,14 @@
 package org.cryptomator.data.db
 
 import android.content.Context
+import android.content.ContextWrapper
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
+import org.cryptomator.data.db.migrations.legacy.Upgrade0To1
 import org.cryptomator.data.db.migrations.legacy.Upgrade10To11
 import org.cryptomator.data.db.migrations.legacy.Upgrade11To12
 import org.cryptomator.data.db.migrations.legacy.Upgrade1To2
@@ -17,6 +21,9 @@ import org.cryptomator.data.db.migrations.legacy.Upgrade7To8
 import org.cryptomator.data.db.migrations.legacy.Upgrade8To9
 import org.cryptomator.data.db.migrations.legacy.Upgrade9To10
 import org.cryptomator.data.db.migrations.manual.Migration12To13
+import org.cryptomator.util.named
+import java.io.File
+import java.nio.file.Files
 import javax.inject.Provider
 import javax.inject.Singleton
 import dagger.Module
@@ -26,12 +33,15 @@ import timber.log.Timber
 @Module
 class DatabaseModule {
 
+	private val DATABASE_NAME = "Cryptomator"
+	private val LOG = Timber.Forest.named("DatabaseModule")
+
 	@Singleton
 	@Provides
 	fun provideCryptomatorDatabase(context: Context, migrations: Array<Migration>): CryptomatorDatabase {
 		Timber.tag("Database").i("Building database (target version: %s)", CRYPTOMATOR_DATABASE_VERSION)
-		return Room.databaseBuilder(context, CryptomatorDatabase::class.java, "Cryptomator") //
-			.createFromAsset(BASE_DATABASE_ASSET) //
+		return Room.databaseBuilder(context, CryptomatorDatabase::class.java, DATABASE_NAME) //
+			.createFromFile(createDbTemplate(context))
 			.addMigrations(*migrations) //
 			.addCallback(DatabaseCallback) //
 			.build() //Fails if no migration is found (especially when downgrading)
@@ -41,6 +51,28 @@ class DatabaseModule {
 				require(it.openHelper.writableDatabase.version == CRYPTOMATOR_DATABASE_VERSION)
 				Timber.tag("Database").i("Database built successfully")
 			}
+	}
+
+	private fun createDbTemplate(context: Context): File {
+		val delegatingContext = object : ContextWrapper(context) {
+			override fun getDatabasePath(name: String?): File {
+				return Files.createTempDirectory(context.cacheDir.toPath(), "BaseDb").resolve(name).toFile()
+			}
+		}
+		val db = SupportSQLiteOpenHelper.Configuration.builder(delegatingContext) //
+			.name(DATABASE_NAME) //
+			.callback(object : SupportSQLiteOpenHelper.Callback(1) {
+				override fun onCreate(db: SupportSQLiteDatabase) {
+					Upgrade0To1().migrate(db)
+				}
+
+				override fun onUpgrade(db: SupportSQLiteDatabase, oldVersion: Int, newVersion: Int) = Unit
+			}).build()
+			.let { FrameworkSQLiteOpenHelperFactory().create(it).writableDatabase }
+		require(db.version == 1)
+		db.close()
+
+		return File(requireNotNull(db.path))
 	}
 
 	@Singleton
