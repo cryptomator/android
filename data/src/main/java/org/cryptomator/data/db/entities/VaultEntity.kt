d15d3b2e8bcb2f41cb897823c7515d566a46a037
JaniruTEC <52893617+JaniruTEC@users.noreply.github.com>
2024-01-11 18:07:01 +0100

    Added explicit orders to VaultEntity at current version

--- data/src/main/java/org/cryptomator/data/db/entities/VaultEntity.kt
+++ data/src/main/java/org/cryptomator/data/db/entities/VaultEntity.kt
@@ -3,11 +3,12 @@ package org.cryptomator.data.db.entities
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
+import androidx.room.Index.Order
 import androidx.room.PrimaryKey
 
 @Entity(
 	tableName = "VAULT_ENTITY", //
-	indices = [Index(name = "IDX_VAULT_ENTITY_FOLDER_PATH_FOLDER_CLOUD_ID", value = ["folderPath", "folderCloudId"], unique = true)], //
+	indices = [Index(name = "IDX_VAULT_ENTITY_FOLDER_PATH_FOLDER_CLOUD_ID", value = ["folderPath", "folderCloudId"], orders = [Order.ASC, Order.ASC], unique = true)], //
 	foreignKeys = [ForeignKey(CloudEntity::class, ["id"], ["folderCloudId"], onDelete = ForeignKey.RESTRICT)]
 )
 data class VaultEntity(
