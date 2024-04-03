8b5733fcf816009c21cc2b9ad8fe44b8ab4ce71b
Steven Brown <35676387+brownowski@users.noreply.github.com>
2024-04-04 08:20:02 +1000

    Update ImagePreviewFragment.kt
    Changed from ARGB_8888 to HARDWARE for performance improvements. Quality remains the same as ARGB_8888.

--- presentation/src/main/java/org/cryptomator/presentation/ui/fragment/ImagePreviewFragment.kt
+++ presentation/src/main/java/org/cryptomator/presentation/ui/fragment/ImagePreviewFragment.kt
@@ -65,7 +65,7 @@ class ImagePreviewFragment : Fragment() {
 			imagePreviewFile.uri?.let {
 				hideProgressBar()
 				imageView.orientation = SubsamplingScaleImageView.ORIENTATION_USE_EXIF
-				SubsamplingScaleImageView.setPreferredBitmapConfig(Bitmap.Config.ARGB_8888)
+				SubsamplingScaleImageView.setPreferredBitmapConfig(Bitmap.Config.HARDWARE)
 				showImage(imagePreviewFile)
 			} ?: presenter.onMissingImagePreviewFile(imagePreviewFile)
 		}
