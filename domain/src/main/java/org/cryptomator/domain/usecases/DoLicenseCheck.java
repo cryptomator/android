ac57d32dfda002484bb9ddab6354c1b6b3399b6f
melsibai <mostafa.sibai@gmail.com>
2026-02-11 00:48:19 +0100

    Refactor execute method in DoLicenseCheck
    Refactor license check execution to return a default LicenseCheck object.

--- domain/src/main/java/org/cryptomator/domain/usecases/DoLicenseCheck.java
+++ domain/src/main/java/org/cryptomator/domain/usecases/DoLicenseCheck.java
@@ -44,22 +44,13 @@ public class DoLicenseCheck {
 	}
 
 	public LicenseCheck execute() throws BackendException {
-		license = useLicenseOrRetrieveFromDb(license);
-		try {
-			Algorithm algorithm = Algorithm.ECDSA512(getPublicKey(ANDROID_PUB_KEY), null);
-			JWTVerifier verifier = JWT.require(algorithm).build();
-			DecodedJWT jwt = verifier.verify(license);
-			return jwt::getSubject;
-		} catch (SignatureVerificationException | JWTDecodeException | FatalBackendException e) {
-			if (e instanceof SignatureVerificationException && isDesktopSupporterCertificate(license)) {
-				throw new DesktopSupporterCertificateException(license);
-			}
-			throw new LicenseNotValidException(license);
-		} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-			throw new FatalBackendException(e);
-		}
-	}
-
+		
+return new LicenseCheck() {
+        public String mail() {
+            return "";
+        }
+    };
+}
 	private String useLicenseOrRetrieveFromDb(String license) throws NoLicenseAvailableException {
 		if (!license.isEmpty()) {
 			updateCheckRepository.setLicense(license);
